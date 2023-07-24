dac3513f2a720fa145a976e0ccc95e9deeb77d67
stDem <61629062+stDem@users.noreply.github.com>
2023-07-24 23:36:10 +0300

    finished s_1_Homework_1

---
+++ s_1/script.js
@@ -0,0 +1,38 @@
+// console.log('Текст из script.js!');
+// let num = 123;
+// console.log(num);
+// /*Переоперделение 
+// переменной*/
+// let a = 5;
+// a = 7;
+// console.log(a);
+// let b = 0;
+// console.log(b = 1 + 2 + 3);//вывод b
+// //---------------------------------
+// const c = 10;
+// const d = 2;
+// let addition = c + d;
+// let substraction = c - d;
+// let multiplication = c * d;
+// let devision = c / d;
+// console.log(`c + d = ${addition}, c - d = ${substraction}, c * d = ${multiplication}, c / d = ${devision}`);
+// console.log(`Сумма всех элементов = ${addition + substraction + multiplication + devision}`);
+// //--------------------------------
+// const q = 1.5;
+// const w = 0.75;
+// console.log(`Сумма двух элементов: ${q + w}`);
+// let e = -4;
+// console.log(-e);
+// //---------------------------------
+// const givenName = 'Anst';
+// const surname = 'Dem';
+// console.log(`Given name: ${givenName} 
+// Surname: ${surname}`);
+// const java = 'Java';
+// const script = 'Script';
+// console.log(java + script);
+// const hello = 'Hello';
+// const world = 'World';
+// console.log(`${hello} ${world}`);
+
+
