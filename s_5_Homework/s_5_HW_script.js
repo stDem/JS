21e6ca87cd1e484b1145f05869311ab32b61cc2f
stDem <61629062+stDem@users.noreply.github.com>
2023-08-10 00:12:18 +0300

    finished HW_5

---
+++ s_5_Homework/s_5_HW_script.js
@@ -0,0 +1,133 @@
+//Задание 1. Дан объект numbers. Необходимо в консоль вывести все значения больше или равные 3.
+const numbers =
+{
+    keyin1: 1,
+    keyin2: 2,
+    keyin3: 3,
+    keyin4: 4,
+    keyin5: 5,
+    keyin6: 6,
+    keyin7: 7,
+    getNumbersMoreThree() {
+        const numbersMoreThree = {};
+        let i = 0;
+        for (const key in numbers) {
+            if (numbers[key] >= 3) {
+                numbersMoreThree[i] = numbers[key];
+            }
+            i++;
+        }
+        return numbersMoreThree;
+    }
+}
+const numbersMoreThree = numbers.getNumbersMoreThree();
+for (const key in numbersMoreThree)
+    console.log(numbersMoreThree[key]);
+
+//---------------------------------------------------------------------------------------------------
+// Задание 2. Необходимо из объекта, который лежит в константе post вывести значения, 
+// к которым приписан комментарий, в консоль.
+const post =
+{
+    author: "John", // вывести этот текст
+    postId: 23,
+    comments:
+        [
+            {
+                userId: 10,
+                userName: "Alex",
+                text: "lorem ipsum",
+                rating: {
+                    likes: 10,
+                    dislikes: 2, // вывести это число
+                },
+            },
+            {
+                userId: 5, // вывести это число
+                userName: "Jane",
+                text: "lorem ipsum 2", // вывести этот текст
+                rating: {
+                    likes: 3,
+                    dislikes: 1,
+                },
+            },
+        ],
+};
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+//насколько я понимаю можно все варианты записать еще в таком виде?:
+//console.log(post['comments'][0]['rating']['dislikes']);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
+
+//--------------------------------------------------------------------------------------------
+// Задание 3. Дан массив products, необходимо цену каждого продукта уменьшить
+// на 15% используя метод forEach.
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+products.forEach((sale) => console.log(`цена ${sale.id} книги со скидкой = ${sale.price - sale.price * 0.15}`));
+
+//---------------------------------------------------------------------------------
+// Задание 4.
+//1. Необходимо вывести в консоль массив продуктов в котором есть хоть одна
+//  фотография используя метод filter. Исходные данные - массив products.
+// 2. Необходимо отсортировать массив products используя метод sort по цене,
+//  начиная с самой маленькой, заканчивая самой большой ценой, после чего вывести
+//  отсортированный массив в консоль.
+
+const product =
+    [
+        {
+            id: 3,
+            price: 127,
+            photos: ["1.jpg", "2.jpg",],
+        },
+        {
+            id: 5,
+            price: 499,
+            photos: [],
+        },
+        {
+            id: 10,
+            price: 26,
+            photos: ["3.jpg",],
+        },
+        {
+            id: 8,
+            price: 78,
+        },
+    ];
+console.log(product);
+//-------1----------------------
+const photo = product.filter((photoCount => photoCount['photos']));
+console.log(photo);
+//-------2----------------------
+let sortByPrice = (a, b) => a.price > b.price ? 1 : -1;
+product.sort(sortByPrice);
+console.log(product);
+
+
+//------------------------------------------------------------------------------------------
+// **Задание 5** Дано 2 массива. Вам необходимо объединить 2 этих массива, 
+// чтобы значения первого массива были ключами, а значения второго массива — значениями.
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+// const weekDays = {};
+const map = new Map();
+for (let i in ru)
+    map.set(en[i], ru[i]);
+// weekDays[en[i]] = ru[i];
+console.log(map);
